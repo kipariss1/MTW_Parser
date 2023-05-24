c78b9efa6abc44aae32134f975ba42f4f965c2fe
Efim Galliamov <efimgallyamov@yandex.ru>
2023-05-25 02:04:37 +0300

    add feat (main.py): Fetch SIGMET's location
    Add feature that prints coordinates from SIGMET using regular expression. It prints onl first line of coordinates and includes information about movement of phenomenon and its altitude.

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 url = 'https://aviationweather-bldr.ncep.noaa.gov/sigmet/intl?hazard=mtw&loc=all'
 my_html = requests.get(url)
 my_html_text = my_html.text
 soup = BeautifulSoup(my_html_text, 'html.parser')
 
-sigmet_tags = soup.find_all('pre')
+sigmet_group = soup.find('pre')
 
-print(sigmet_tags[0].text)
+sigmet_tags = sigmet_group.find_all('br')
+
+for sigmet_text in sigmet_tags:
+    print(sigmet_text.previous)
+    location = r'WI\s(.+)\n'
+    matches = re.search(location, sigmet_text.previous)
+    location_text = matches.group(1)
+    print('Location: ', location_text + '\n')
+
+
+
+
