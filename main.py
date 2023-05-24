1941688701a70137f055ab50251855f7e2087c70
Efim Galliamov <efimgallyamov@yandex.ru>
2023-05-24 23:52:25 +0300

    add feat (main.py): Fetching and parcing MTW SIGMETs
    Fetching and parcing MTW SIGMETs raw text from url :  https://aviationweather-bldr.ncep.noaa.gov/sigmet/intl?hazard=mtw&loc=all

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print('kiparis')
-print('kiparis')
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://aviationweather-bldr.ncep.noaa.gov/sigmet/intl?hazard=mtw&loc=all'
+my_html = requests.get(url)
+my_html_text = my_html.text
+soup = BeautifulSoup(my_html_text, 'html.parser')
+
+sigmet_tags = soup.find_all('pre')
+
+print(sigmet_tags[0].text)
