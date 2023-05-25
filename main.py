7749fc01da0290735caed367c20e631f440c6a8e
Efim Galliamov <efimgallyamov@yandex.ru>
2023-05-25 15:46:54 +0300

    fix coordinates and add FIR
    Get coordinates output as a tuple of fixes in lat and long format. Add output of FIR name.

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ my_html_text = my_html.text
 soup = BeautifulSoup(my_html_text, 'html.parser')
 
 sigmet_group = soup.find('pre')
-
 sigmet_tags = sigmet_group.find_all('br')
 
-for sigmet_text in sigmet_tags:
-    print(sigmet_text.previous)
-    location = r'WI\s(.+)\n'
-    matches = re.search(location, sigmet_text.previous)
-    location_text = matches.group(1)
-    print('Location: ', location_text + '\n')
+for sigmet in sigmet_tags:
+    sigmet_text = sigmet.previous
+    coordinates_pattern = re.compile(r'[NS]\d{4}\s[WE]\d{5}', re.MULTILINE)
+    matches = [match.group() for match in coordinates_pattern.finditer(sigmet_text)]
+    match = re.search(r'\w{4}\s\w+\sFIR', sigmet_text)
+    print(matches)
+    print(match.group(), '\n')
 
 
 
